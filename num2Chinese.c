136861ec6877eaca72b0182ecbb46dae3d083428
luckydan61 <apple@apple.local>
2017-06-11 08:58:34 +0800

    add number unit test

--- num2Chinese.c
+++ num2Chinese.c
@@ -1,4 +1,6 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 /*
 *	Number to Chinese
 */
@@ -17,7 +19,70 @@ char * chinese[] =
 	"九"
 };
 
+char * numUnit[] = 
+{
+	"",
+	"十",
+	"百",
+	"千",
+	"万",
+	"十万",
+	"百万",
+	"千万",
+	"亿",
+	"十亿",
+	"百亿",
+	"千亿",
+};
+
+void AnalyseNum(int num, int * numArr)
+{
+	int bit = 0;
+	numArr[bit] = num % 10;
+	while(num / 10)
+	{
+		bit++;
+		num /= 10;
+		numArr[bit] = num % 10;
+	}
+}
+
+void ConstructChineseNum(int * numArr, char * chineseNum)
+{
+	int i = 0;
+	int zeroFlag = 0;
+#if 0
+	for(i = 9; i >= 0; i--)
+	{
+		printf("%d", numArr[i]);
+	}
+	printf("\n");
+#endif
+	for(i = 9; i >=0; i--)
+	{
+		if(numArr[i] == 0)
+		{
+			continue;
+		}
+		zeroFlag++;
+		//printf("%s, %s\n", chinese[numArr[i]], numUnit[i]);
+		strcat(chineseNum, chinese[numArr[i]]);
+		strcat(chineseNum, numUnit[i]);
+	}
+	if(zeroFlag == 0)
+	{
+		strcat(chineseNum, chinese[0]);
+	}
+	//printf("1: %s\n", chineseNum);
+}
+
 char * num2Chinese(int num)
 {
-	return chinese[num];	
+	int numArr[10] = {0};
+	char chineseNum[100] = {0};
+
+	AnalyseNum(num, numArr);
+	ConstructChineseNum(numArr, chineseNum);
+	//printf("2: %s\n", chineseNum);
+	return &chineseNum;
 }
--- testNum2Chinese.c
+++ testNum2Chinese.c
@@ -17,9 +17,23 @@ void TestSingleNum()
 	assert(strcmp("九", num2Chinese(9)) == 0);
 }
 
+void TestRoundNumber()
+{
+	assert(strcmp("一十", num2Chinese(10)) == 0);
+	assert(strcmp("一百", num2Chinese(100)) == 0);
+	assert(strcmp("一千", num2Chinese(1000)) == 0);
+	assert(strcmp("一万", num2Chinese(10000)) == 0);
+	assert(strcmp("一十万", num2Chinese(100000)) == 0);
+	assert(strcmp("一百万", num2Chinese(1000000)) == 0);
+	assert(strcmp("一千万", num2Chinese(10000000)) == 0);
+	assert(strcmp("一亿", num2Chinese(100000000)) == 0);
+	assert(strcmp("一十亿", num2Chinese(1000000000)) == 0);
+}
+
 void Test_Num_2_Chinese()
 {
 	printf("Start to test num2Chinese.\n");
 	TestSingleNum();
+	TestRoundNumber();
 	printf("Test num2Chinese ok.\n");
 }
